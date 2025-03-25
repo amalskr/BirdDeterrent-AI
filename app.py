d3c64e00d6dc07b913f30bcc7559c9122132f7f1
Amal Ranasinghe <amal@soho.com.au>
2025-03-25 17:11:13 +0530

    uplaoded image uplode

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import numpy as np
 from flask import Flask, render_template, request
 from tensorflow import keras
 from datetime import datetime
+from multiprocessing import Value
+
+# declare counter variable
+counter = Value('i', 0)
 
 app = Flask(__name__)
 
@@ -61,7 +65,34 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/upload', methods=['POST'])
+def save_img(img):
+    with counter.get_lock():
+        counter.value += 1
+        count = counter.value
+    img_dir = "esp32_imgs"
+    if not os.path.isdir(img_dir):
+        os.mkdir(img_dir)
+    cv2.imwrite(os.path.join(img_dir,"img_"+str(count)+".jpg"), img)
+# print("Image Saved", end="\n") # debug
+
+@app.route('/upload', methods=['POST','GET'])
+def upload():
+    received = request
+    img = None
+    if received.files:
+        print(received.files['imageFile'])
+        # convert string of image data to uint8
+        file  = received.files['imageFile']
+        nparr = np.fromstring(file.read(), np.uint8)
+        # decode image
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        save_img(img)
+
+        return "[SUCCESS] Image Received", 201
+    else:
+        return "[FAILED] Image Not Received", 204
+
+@app.route('/uploadOld', methods=['POST'])
 def upload_image():
     if 'imageFile' not in request.files:
         return '❌ No imageFile field in request', 400
